26911a4ee4848bd973a3650e4958365fc88d1c14
yixinchang <572352114@qq.com>
2022-08-15 19:17:58 +0800

    v1.0

--- reggie_take_out/src/main/java/com/itheima/reggie/ReggieApplication.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -7,19 +7,20 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
+/** @SpringBootApplication: 告诉Spring 这是一个Springboot应用*/
 @SpringBootApplication
+/** @ServletComponentScan:
+ SpringBootApplication 上使用@ServletComponentScan 注解后
+ Servlet可以直接通过@WebServlet注解自动注册
+ Filter可以直接通过@WebFilter注解自动注册
+ Listener可以直接通过@WebListener 注解自动注册*/
 @ServletComponentScan
-/*
-@ServletComponentScan:
-    SpringBootApplication 上使用@ServletComponentScan 注解后
-    Servlet可以直接通过@WebServlet注解自动注册
-    Filter可以直接通过@WebFilter注解自动注册
-    Listener可以直接通过@WebListener 注解自动注册
-* */
+
 @EnableTransactionManagement
 //Spring Boot使用事务非常简单，首先使用注解 @EnableTransactionManagement 开启事务支持后，然后在访问数据库的Service方法上添加注解 @Transactional 便可
 public class ReggieApplication {
     public static void main(String[] args){
+        //固定写法
         SpringApplication.run(ReggieApplication.class,args);
         log.info("项目成功启动了！");
     }
